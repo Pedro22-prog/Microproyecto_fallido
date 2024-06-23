88668ad93483442f9a6613d96bfc0a18f5e672f7
Pedro22-prog <pedro.jgonzalez73@gmail.com>
2024-06-23 16:29:37 -0400

    Se borro App.js y se paso el codigo a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,3 +20,23 @@ const App = () => {
 };
 
 export default App;
+//import React from 'react';
+//import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//import ProtectedRoute from './ProtectedRoute';
+//import Dashboard from './Dashboard';
+//import Login from './Login';
+//import Register from './Register';
+
+//const App = () => {
+//  return (
+//    <BrowserRouter>
+//      <Switch>
+//        <Route path="./Login" component={Login} />
+//        <Route path="./Register" component={Register} />
+//        <ProtectedRoute path="/" component={Dashboard} />
+//      </Switch>
+//    </BrowserRouter>
+//  );
+//};
+
+//export default App;
